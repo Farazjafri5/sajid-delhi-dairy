03907be0497a4477cd569e31e824c48f75021aed
Farazjafri5 <jafrifaraz4@gmail.com>
2026-08-24 17:13:10 +0530

    CMS Update src/data/siteContent.ts from Admin Panel

--- src/data/siteContent.ts
+++ src/data/siteContent.ts
@@ -19,7 +19,7 @@ export const defaultSiteContent: SiteContent = {
     ]
   },
   "showreel": {
-    "heading": "This is what we do. ",
+    "heading": "This is what we do. 34233",
     "description": "We shoot short-form videos designed to convert casual scrollers into loyal customers. No stock templates, no boring structures.",
     "leftImages": [
       {
